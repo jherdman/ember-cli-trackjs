88436bf2cc62a10a13c1cd05f31253c3ecb14382
James Herdman <james.herdman@gmail.com>
2015-03-30 16:48:48 -0400

    Flesh out Fake TrackJS Implementation
    Doing this fixed a lot of weirdo errors I was seeing before. This also
    allowed me to use the TrackJS Service directly inside of the
    configuration initializer instead of having to reach out to the global.

--- tests/dummy/public/fake-trackjs.js
+++ tests/dummy/public/fake-trackjs.js
@@ -1,3 +1,25 @@
-/**
- * I don't actually want to implement this, it throws off all of the other tests
- */
+window.trackJs = {
+  configure: function(newOpts) {
+    console.log("Configuring");
+
+    var originalOpts = window._trackJs;
+    var modifiedOpts = Ember.merge(originalOpts, newOpts);
+
+    window._trackJs = modifiedOpts;
+  },
+
+  _errors: [],
+
+  track: function(errorOrString) {
+    console.error(errorOrString);
+    this._errors.push(errorOrString);
+  },
+
+  _reset: function() { this.errors = []; },
+
+  attempt: function() { console.log("Attempting"); },
+
+  watch: function() { console.log("Watching"); },
+
+  watchAll: function() { console.log("Watching all"); }
+};
