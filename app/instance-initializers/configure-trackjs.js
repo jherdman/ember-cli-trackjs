bda8b1ad92da558a6a901257aa29d93d1cec880f
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2015-12-03 16:37:34 +0300

    Correctly Handle Ember Errors

--- app/instance-initializers/configure-trackjs.js
+++ app/instance-initializers/configure-trackjs.js
@@ -8,24 +8,44 @@ export function initialize(application) {
     version: appVersion
   });
 
-  // http://docs.trackjs.com/Examples/Integrating_with_Ember
-  Ember.onerror = function (err) {
-    if (err.name === 'TransitionError') {
-      Ember.debug('Ignoring TransitionError:', err);
-    } else {
-      trackJs.track(err);
-      Ember.Logger.assert(false, err);
-    }
-  };
+  Ember.onerror = function(error) {
+    // If you pass a POJO to TrackJS, you'll see "[object Object]"
+    // in reports instead of object content. :(
 
-  Ember.RSVP.on('error', function (err) {
-    if (err.name === 'TransitionError') {
-      Ember.debug('Ignoring TransitionError:', err);
-    } else {
-      trackJs.track(err);
-      Ember.Logger.assert(false, err);
+    // If the error is an Error object, we pass it directly.
+    if (error instanceof Error) {
+      trackJs.track(error);
+      Ember.Logger.error(error.stack);
+      return;
     }
-  });
+
+    // Otherwise, we have to serialize it manually in order
+    // to see something better than "[object Object]".
+    let serializedError;
+
+    try {
+      // This will work for both primitives and POJOs:
+      serializedError = JSON.stringify(error, null, 2);
+    } catch (e) {
+      // Protecting against circular reference errors
+      if (e.message = "Converting circular structure to JSON") {
+        // We have an object with circular references and thus
+        // we can't serialize it into JSON. We have to extract
+        // info from it manually.
+        serializedError =
+          `Object with circular references passed instead of an instance of Error.
+            error.message: ${error.message}
+            error.name:    ${error.name}
+          `;
+      } else {
+        // Some other error happened in JSON.stringify, rethrowing.
+        throw e;
+      }
+    }
+
+    trackJs.track(serializedError);
+    Ember.Logger.error(error);
+  };
 }
 
 export default {
