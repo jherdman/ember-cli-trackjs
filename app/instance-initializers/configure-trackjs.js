fafd680e053b1a4d5862c42e8eda74fb2c48a14f
Andrey Mikhaylov (lolmaus) <lolmaus@gmail.com>
2015-12-03 16:37:34 +0300

    Correctly handle Ember errors

--- app/instance-initializers/configure-trackjs.js
+++ app/instance-initializers/configure-trackjs.js
@@ -1,34 +1,52 @@
 import Ember from 'ember';
 
 export function initialize(application) {
-  let trackJs = application.container.lookup('service:trackjs');
-  let appVersion = application.container.lookup('application:main').get('version');
+  const trackJs = application.container.lookup('service:trackjs');
 
   trackJs.configure({
-    version: appVersion
+    version: application.container.lookup('application:main').get('version')
   });
 
-  // http://docs.trackjs.com/Examples/Integrating_with_Ember
-  Ember.onerror = function (err) {
-    if (err.name === 'TransitionError') {
-      Ember.debug('Ignoring TransitionError:', err);
-    } else {
-      trackJs.track(err);
-      Ember.Logger.assert(false, err);
+  Ember.onerror = error => {
+    // If you pass a POJO to TrackJS, you'll see "[object Object]"
+    // in reports instead of object content. :(
+
+    // If the error is an Error object, we pass it directly.
+    if (error instanceof Error) {
+      trackJs.track(error);
+      Ember.Logger.error(error.stack);
+      return;
     }
+
+    // Otherwise, we have to serialize it manually in order
+    // to see something better than "[object Object]".
+    let serializedError;
+    try {
+      // This will work for both primitives and POJOs:
+      serializedError = JSON.stringify(error, null, 2);
+    } catch (e) {
+      // Protecting against circular reference errors
+      if (e.message = "Converting circular structure to JSON") {
+        // We have an object with circular references and thus
+        // we can't serialize it into JSON. We have to extract
+        // info from it manually.
+        serializedError =
+          `Object with circular references passed instead of an instance of Error.
+            error.message: ${error.message}
+            error.name:    ${error.name}
+          `;
+      } else {
+        // Some other error happened in JSON.stringify, retrhowing.
+        throw e;
+      }
+    }
+
+    trackJs.track(serializedError);
+    Ember.Logger.error(error);
   };
-
-  Ember.RSVP.on('error', function (err) {
-    if (err.name === 'TransitionError') {
-      Ember.debug('Ignoring TransitionError:', err);
-    } else {
-      trackJs.track(err);
-      Ember.Logger.assert(false, err);
-    }
-  });
 }
 
 export default {
-  name: 'configure-trackjs',
+  name:       'configure-trackjs',
   initialize: initialize
 };
