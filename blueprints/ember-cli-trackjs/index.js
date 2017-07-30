701872267919c10769e7ad7a01bed8caadb7a605
James Herdman <james.herdman@gmail.com>
2017-07-30 12:48:19 -0400

    Remove Bower Installation

--- blueprints/ember-cli-trackjs/index.js
+++ blueprints/ember-cli-trackjs/index.js
@@ -5,10 +5,4 @@ module.exports = {
   normalizeEntityName: function() {
     // Silence error. We don't actually care about this.
   },
-
-  afterInstall: function() {
-    return this.addBowerPackagesToProject([
-      { name: 'trackjs', target: '~2.6.2' }
-    ]);
-  }
 };
