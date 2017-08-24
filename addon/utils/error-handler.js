e0a6ff0fb65cbfe6daceffc94c3db09c19c57d8e
Wayne Taylor <wayne.taylor@britishgas.co.uk>
2017-08-24 16:13:07 +0100

    UDSS-957 Fix includes usage for IE

--- addon/utils/error-handler.js
+++ addon/utils/error-handler.js
@@ -7,7 +7,7 @@ const getStack = error => {
   let stack = error.stack;
   let message = error.message;
 
-  if (stack && !stack.includes(message)) {
+  if (stack && !stack.indexOf(message) > -1) {
     stack = `${message}\n${stack}`;
   }
 
