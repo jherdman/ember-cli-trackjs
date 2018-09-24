ad5b1df716f81d7b31f9849320d7b5c7fbecf3ad
James Herdman <james.herdman@gmail.com>
2018-09-24 13:25:41 -0400

    Remove CDN Option
    Resolves #84

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ module.exports = {
     let trackOpts;
     let trackConfig;
     let trackConfiguration;
-    let trackBoilerPlate;
 
     if (type === 'head-footer') {
       trackOpts = config.trackJs || {};
@@ -15,21 +14,13 @@ module.exports = {
 
       trackConfiguration = `<script type="text/javascript" id="trackjs-configuration">window._trackJs = ${JSON.stringify(trackConfig)};</script>`;
 
-      if (trackOpts.url) {
-        trackBoilerPlate = `<script type="text/javascript" id="trackjs-boilerplate" src="${trackOpts.url}" crossorigin="anonymous"></script>`;
-      }
-
-      return [trackConfiguration, trackBoilerPlate].join('\n');
+      return trackConfiguration;
     }
   },
 
   included(app) {
     this._super.included(app);
 
-    let options = app.options['ember-cli-trackjs'];
-
-    if (!(options && options.cdn) && (!process.env.EMBER_CLI_FASTBOOT)) {
-      app.import(require.resolve('trackjs'));
-    }
+    app.import(require.resolve('trackjs'));
   }
 };
